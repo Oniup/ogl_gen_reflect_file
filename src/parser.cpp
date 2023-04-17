8ecfca18cf86cc1f4c8287aa63c404d68784ad24
Ewan <ewanrobsonn@gmail.com>
2023-04-18 01:05:38 +1000

    added license and started parsing keywords or gen_reflection system

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,3 +1,22 @@
+/****************************************************************************/
+/* parser.cpp                                                               */
+/* This file is part of: ogl_gen_reflection_file                            */
+/****************************************************************************/
+/* Copyright 2023 Ewan Robson                                               */
+/*                                                                          */
+/* Licensed under the Apache License, Version 2.0 (the "License");          */
+/* you may not use this file except in compliance with the License.         */
+/* You may obtain a copy of the License at                                  */
+/*                                                                          */
+/*     http://www.apache.org/licenses/LICENSE-2.0                           */
+/*                                                                          */
+/* Unless required by applicable law or agreed to in writing, software      */
+/* distributed under the License is distributed on an "AS IS" BASIS,        */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. */
+/* See the License for the specific language governing permissions and      */
+/* limitations under the License.                                           */
+/****************************************************************************/
+
 #include "parser.hpp"
 #include "gen_reflection_file.hpp"
 
@@ -39,12 +58,12 @@ ParserObject& ParserObject::operator=(ParserObject&& other) {
 
 ParserHandler* parse_reflection_files(ParserHandler* handler, InputArgs* input_args) {
     if (handler == nullptr) {
-        print_debug(input_args, "[ERROR: ReflectParser* parse_reflection_files(ReflectParser*, InputArgs*)] --> ParserHandler is null\n");
+        print_debug(input_args, "[ERROR: ParserHandler* parse_reflection_files(ReflectParser*, InputArgs*)] --> ParserHandler is null\n");
         return nullptr;
     }
 
     if (input_args->read_file_paths.size() == 0) {
-        print_debug(input_args, "[ERROR: ReflectParser* parse_reflection_file(ReflectParser*, InputArgs*)] --> InputArg::read_file_path.size() == 0\n");
+        print_debug(input_args, "[ERROR: ParserHandler* parse_reflection_file(ReflectParser*, InputArgs*)] --> InputArg::read_file_path.size() == 0\n");
 
         delete handler;
         return nullptr;
@@ -53,29 +72,37 @@ ParserHandler* parse_reflection_files(ParserHandler* handler, InputArgs* input_a
     for (const char* path : input_args->read_file_paths) {
         std::FILE* file = std::fopen(path, "rb");
         if (file == nullptr) {
-            print_debug(input_args, std::string("[ERROR: ReflectParser* parse_reflection_file(ReflectParser*, InputArgs*)] --> failed to open file with path: ") + path + "\n");
+            print_debug(input_args, std::string("[ERROR: ParserHandler* parse_reflection_file(ReflectParser*, InputArgs*)] --> failed to open file with path: ") + path + "\n");
 
             delete handler;
             return nullptr;
         }
 
-        int line_number = 0;
         while (std::fgets(handler->line, handler->line_size, file) != nullptr) {
             std::string keyword{};
             size_t offset = 0;
             size_t line_size = strlen(handler->line);
-            size_t keyword_count = 0;
             while (parse_construct_keyword_from_line(keyword, offset, handler->line, line_size)) {
-                print_debug(input_args, keyword + "\n");
-                keyword_count++;
+                // NOTE: doesn't work
+                if (!parse_keyword(handler, keyword)) {
+                    print_debug(
+                        input_args, 
+                        std::string("[ERROR: ParserHandler* parse_reflection_file(ReflectParser*, InputArgs*)] --> parse_keyword(ParserHandler*, std::string) failed on keyword: ") + 
+                        keyword + "\n"
+                    );
+
+                    std::fclose(file);
+                    delete handler;
+                    return nullptr;
+                }
             }
 
-            line_number++;
-            print_debug(
-                input_args, 
-                std::string("******finished line ") + std::to_string(line_number) + ", keyword count: " + 
-                std::to_string(keyword_count) + ", keyword size: " + std::to_string(keyword.size()) + "******\n"
-            );
+            // line_number++;
+            // print_debug(
+            //     input_args, 
+            //     std::string("******finished line ") + std::to_string(line_number) + ", keyword count: " + 
+            //     std::to_string(keyword_count) + ", keyword size: " + std::to_string(keyword.size()) + "******\n"
+            // );
         }
 
         std::fclose(file);
@@ -86,8 +113,8 @@ ParserHandler* parse_reflection_files(ParserHandler* handler, InputArgs* input_a
 
 bool parse_construct_keyword_from_line(std::string& keyword, size_t& offset, const char* code_line, size_t code_line_size) {
     constexpr size_t breaking_characters_include_in_keyword_index = 4;
-    constexpr std::array<char, 7> breaking_characters {
-        ' ', '\n', '\r', '\t', ',', '<', '>'
+    constexpr std::array<char, 9> breaking_characters {
+        ' ', '\n', '\r', '\t', ',', '<', '>', '{', '}'
     };
 
     size_t keyword_size = 0;
@@ -133,28 +160,57 @@ bool parse_construct_keyword_from_line(std::string& keyword, size_t& offset, con
     return false;
 }
 
-void parse_keyword(ParserHandler* handler, const std::string& keyword) {
+bool parse_keyword(ParserHandler* handler, const std::string& keyword) {
+    bool has_end_scope = false;
+    if (keyword == "}") {
+        parser_end_scope(handler);
+    }
+    else if (keyword.back() == '}') {
+        has_end_scope = true;
+    }
+
+    if (handler->next_keyword_open_scope) {
+        handler->next_keyword_open_scope = false;
+        return keyword == "{";
+    }
+
     switch (handler->current_process) {
     case ParsingProcess_FindTarget:
         if (keyword == "namespace") {
-            handler->current_process = ParsingProcess_Namespace;
+            handler->current_process = ParsingProcess_NamespaceName;
         }
         else if (keyword == "struct" || keyword == "class") {
-            handler->current_process = ParsingProcess_Object;
+            handler->current_process = ParsingProcess_ObjectName;
+        }
+        else if (keyword == "struct{") {
+            parser_push_scope(handler, ParserScope {});
+            handler->current_process = ParsingProcess_ObjectContent;
         }
         else if (keyword == "template" || keyword == "template<") {
             handler->current_process = ParsingProcess_Template;
         }
 
         return;
-    case ParsingProcess_Namespace:
-        
+
+    case ParsingProcess_NamespaceName:
+        parser_push_scope(handler, ParserScope { keyword });
+
+        handler->current_process = ParsingProcess_FindTarget;
+        if (keyword.back() != '{') {
+            handler->next_keyword_open_scope = true;
+        }
+
+        break;
+
+    case ParsingProcess_ObjectName:
         break;
     }
 
-    if (keyword == "{") {
+    if (has_end_scope) {
+        return parser_end_scope(handler);
     }
 
+    return true;
 }
 
 bool remove_object_from_parser(ParserHandler* handler) {
@@ -162,5 +218,25 @@ bool remove_object_from_parser(ParserHandler* handler) {
         return false;
     }
 
+    return true;
+}
+
+void parser_push_scope(ParserHandler* handler, ParserScope&& new_scope) {
+    if (handler->current_scope != nullptr) {
+        handler->current_scope->scopes.emplace_back(std::move(new_scope));
+        handler->current_scope = &handler->current_scope->scopes.back();
+    }
+    else {
+        handler->scopes.emplace_back(std::move(new_scope));
+        handler->current_scope = &handler->scopes.back();
+    }
+}
+
+bool parser_end_scope(ParserHandler* handler) {
+    if (handler->current_scope == nullptr) {
+        return false;
+    }
+
+    handler->current_scope = handler->current_scope->previous_scope;
     return true;
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,3 +1,22 @@
+/****************************************************************************/
+/* parser.hpp                                                               */
+/* This file is part of: ogl_gen_reflection_file                            */
+/****************************************************************************/
+/* Copyright 2023 Ewan Robson                                               */
+/*                                                                          */
+/* Licensed under the Apache License, Version 2.0 (the "License");          */
+/* you may not use this file except in compliance with the License.         */
+/* You may obtain a copy of the License at                                  */
+/*                                                                          */
+/*     http://www.apache.org/licenses/LICENSE-2.0                           */
+/*                                                                          */
+/* Unless required by applicable law or agreed to in writing, software      */
+/* distributed under the License is distributed on an "AS IS" BASIS,        */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. */
+/* See the License for the specific language governing permissions and      */
+/* limitations under the License.                                           */
+/****************************************************************************/
+
 #ifndef __parser_HPP__
 #define __parser_HPP__
 
@@ -10,14 +29,16 @@
 
 enum ParsingProcess {
     ParsingProcess_FindTarget,
-    ParsingProcess_Namespace,
+    ParsingProcess_NamespaceName,
     ParsingProcess_Template,
-    ParsingProcess_Object,
+    ParsingProcess_TemplateParameter,
     ParsingProcess_TemplateWithinObject,
-    ParsingProcess_ObjectMember,
-    ParsingProcess_ObjectMemberPtrFunc,
+    ParsingProcess_ObjectName,
+    ParsingProcess_ObjectContent,
+    ParsingProcess_ObjectFunction,
     ParsingProcess_NoSerializable,
     ParsingProcess_NoReflection,
+    ParsingProcess_Function,
 };
 
 struct ParserObjectMember {
@@ -50,6 +71,8 @@ struct ParserObject {
 struct ParserScope {
     std::string namespace_name{};
     ParserObject* object{ nullptr };
+    std::vector<ParserScope> scopes{};
+    ParserScope* previous_scope{ nullptr };
 };
 
 struct ParserHandler {
@@ -57,18 +80,22 @@ struct ParserHandler {
     char line[line_size]{};
     std::string current_namespace{};
     ParserObject object{};
-    std::vector<ParserScope> scope{};
+    std::vector<ParserScope> scopes{};
+    ParserScope* current_scope{ nullptr };
+    bool next_keyword_open_scope{ false };
     struct GenReflection* gen_reflection_handler{ nullptr };
 
     ParsingProcess current_process{ ParsingProcess_FindTarget };
-    bool add_reflect_object{ false };
     std::vector<std::tuple<std::string, ParserObject>> objects{};
 };
 
 ParserHandler* parse_reflection_files(ParserHandler* handler, InputArgs* input_args);
 
 bool parse_construct_keyword_from_line(std::string& keyword, size_t& offset, const char* code_line, size_t code_line_size);
-void parse_keyword(ParserHandler* handler, const std::string& keyword);
+bool parse_keyword(ParserHandler* handler, const std::string& keyword);
 bool remove_object_from_parser(ParserHandler* handler);
+
+void parser_push_scope(ParserHandler* handler, ParserScope&& new_scope);
+bool parser_end_scope(ParserHandler* handler);
 
 #endif
